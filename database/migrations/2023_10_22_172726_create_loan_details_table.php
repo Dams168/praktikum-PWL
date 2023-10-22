db46c71221c359b1524afbe395a60c8107877a09
Dams168 <adamrohman2003@gmail.com>
2023-10-23 00:45:10 +0700

    first Commit, add migration + Seeding

---
+++ database/migrations/2023_10_22_172726_create_loan_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loan_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('loan_id');
+            $table->foreign('loan_id')->references('id')->on('loans')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')->references('id')->on('books')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->boolean('is_return');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loan_details');
+    }
+};
